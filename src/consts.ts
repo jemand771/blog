6b029042f9ffc24650d85a5a693f64a504eb2b8e
Willy <jemand771@gmx.net>
2024-03-18 23:05:56 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "willy's blog";
+export const SITE_DESCRIPTION = "A blog about myself and my dev adventures";
