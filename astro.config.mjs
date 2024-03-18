d057827f12761f2a90d7dd82998a843306aa4e37
Willy <jemand771@gmx.net>
2024-03-19 00:27:53 +0100

    use astro-icon instead of inline svgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jemand771.net',
-    integrations: [mdx(), sitemap()],
+  site: 'https://jemand771.net',
+  integrations: [mdx(), sitemap(), icon()]
 });
